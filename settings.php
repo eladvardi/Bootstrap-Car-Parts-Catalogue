54f640eaa63cdbf90892b183c8ddadf37f0b63a7
eladvardi <elad.vardi@gmail.com>
2016-11-06 01:17:05 +0200

    more url updates

--- settings.php
+++ settings.php
@@ -5,9 +5,6 @@ require_once "models/mysql.model.php";
 $global_conditions =  array(0=>'Good',1=>'As new',2=>'Bad',3=>'New in a Box');
 
 $admin = 0;
-if (isset($_GET['admin']) && ($_GET['admin']=='1') ){
-	$admin = 1;
-}
 
 //  Checking post data for action of insert or delete
 if ( isset($_POST['action']) && ($_POST['action']=='insert') ){
@@ -24,17 +21,14 @@ check_post_purchase();
 
 function get_page(){
 
-	$page = array_shift(explode('/', trim($_SERVER['PATH_INFO'],'/')));
+	$valid_pages = array('best-sales','sales','cars','admin');
 
-
-	if (trim($page)=='') {
+	if (!isset($_GET['page'])) {
 		return 'index';
 	}
 
-	$valid_pages = array('best-sales','sales','cars','admin');
-
-	if (in_array($page,$valid_pages)){
-		return $page;
+	if (in_array($_GET['page'],$valid_pages)){
+		return $_GET['page'];;
 	}
 }
 
