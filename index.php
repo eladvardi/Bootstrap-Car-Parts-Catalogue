d7ddd6ede971d865b2bc2f27c0b1ccf389c136f0
eladvardi <elad.vardi@gmail.com>
2016-11-11 00:43:40 +0200

    responsive tables + fork ribbon

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ print_top();
 switch (get_page()) {
 
 	case 'index':
-		echo create_search_bar('Search a car part in stock');
+		echo create_search_bar('Search a car part');
 		echo purchase_box();
 		echo print_results_table(get_parts($_GET['q']),$admin);
 		break;
@@ -33,6 +33,20 @@ switch (get_page()) {
 		break;
 
 	case 'cars':
+	
+	echo '
+	<div class="modal fade" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">              
+      <div class="modal-body">
+      	<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <img src="" class="imagepreview" style="width: 100%;" >
+      </div>
+    </div>
+  </div>
+</div>
+	';
+	
 		echo page_title("Cars Catalogue",2);
 		echo print_cars_table(get_car_list());
 		break;
